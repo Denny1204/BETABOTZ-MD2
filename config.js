38304b94ea7e0be5e936f6be6fc7ac4e6c423eed
Denny1204 <93825565+Denny1204@users.noreply.github.com>
2024-03-28 23:51:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['6285842647866']  
+global.owner = ['6283866757153']  
 global.mods = ['6285842647866'] 
-global.prems = ['6285842647866']
-global.nameowner = 'Lann'
-global.numberowner = '6285842647866'
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/G4f1fTpz9zL4EH3FyIcaPR'
-global.instagram = 'https://instagram.com/erlanrahmat_14'
-global.wm = '© Tio'
+global.prems = ['6283866757153']
+global.nameowner = 'Katyusha'
+global.numberowner = '6283866757153'
+global.mail = 'kontol@gmail.com' 
+global.gc = 'https://chat.whatsapp.com/IxMc9vbRXmu0yVcKYuQ1SH'
+global.instagram = 'https://instagram.com/hashiro_97'
+global.wm = '© Katyusha'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -15,11 +15,11 @@ global.author = 'Bot WhatsApp'
 global.maxwarn = '2' // Peringatan maksimum
 
 //INI WAJIB DI ISI!//
-global.lann = 'YOUR_APIKEY_HERE' 
+global.lann = '7bzibdIo' 
 //Daftar terlebih dahulu https://api.betabotz.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
-global.btc = 'YOUR_APIKEY_HERE'
+global.btc = 'OgZzoDKr'
 //Daftar https://api.botcahx.eu.org 
 
 global.APIs = {   
@@ -27,8 +27,8 @@ global.APIs = {
   btc: 'https://api.botcahx.eu.org'
 }
 global.APIKeys = { 
-  'https://api.betabotz.eu.org': 'APIKEY', 
-  'https://api.botcahx.eu.org': 'APIKEY'
+  'https://api.betabotz.eu.org': '7bzibdIo', 
+  'https://api.botcahx.eu.org': 'OgZzoDKr'
 }
 
 let fs = require('fs')
